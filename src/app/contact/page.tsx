7daeb87a7295accf9c5005793a4351ebcab701d5
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-06-19 17:21:45 +0200

    Edited layout send button

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link'
 import './contact.scss'
 /* import Image from 'next/image' */
 import { Metadata } from 'next'
+import ContactForm from '../components/ContactForm'
 
 export const metadata: Metadata = {
   title: "Fluidsep - Contact"
@@ -16,9 +17,19 @@ export default function Contact() {
           <h1>Contact us</h1>
         </div>
       </div>
-      <main className="contact-content" >
-        <p className='details'>Email: <Link href="mailto:info@fluidsep.com">info@fluidsep.com</Link><br />
-          Mobile: +47 930 28 690</p>
+      <main>
+        <div className='contact-content'>
+          <div className='contact-details'>
+            <p className='details'>Email: <Link href="mailto:info@fluidsep.com">info@fluidsep.com</Link><br />
+              Mobile: +47 930 28 690</p>
+          </div>
+          <div className='contact-form'>
+            <h2>Send us a message</h2>
+            <div>
+              <ContactForm />
+            </div>
+          </div>
+        </div>
         <div className='location'>
           <p>Find our office:</p>
           <div>
