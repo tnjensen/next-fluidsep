b286b6c22e4d2ed3076e4ab75b67f8d664a5e759
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2026-07-15 08:32:37 +0200

    Redeploy

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -8,7 +8,6 @@ import './blog.scss'
 import getAllPosts from "../../../lib/getAllPosts";
 
 export default async function Blog() {
-  
   const postData = getAllPosts()
   const data = await postData
 
